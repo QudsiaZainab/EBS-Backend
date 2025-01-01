1e3a23193f9cbffe7acd6db1500679bc59489b01
QudsiaZainab <qudsiazainab2002@gmail.com>
2025-01-02 02:02:09 +0500

    server file updated

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
-import express from 'express';
-import cors from 'cors';
+import express from 'express'
+import cors from 'cors'
 import { connectDB } from './config/db.js';
-import 'dotenv/config';
+import 'dotenv/config'
 import authRouter from './routes/authRoutes.js';
 import eventRouter from './routes/eventRoutes.js';
 
-const app = express();
 
-// Middleware
+// app config
+const app = express()
+const port = 4000
+
+// middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors())
 app.use('/uploads', express.static('uploads'));
 
-// DB connection
+
+//db connection
 connectDB();
+
 
 // Use authentication routes
 app.use('/api/auth', authRouter);
 app.use('/api/events', eventRouter);
 
-// Root route
-app.get("/", (req, res) => {
-    res.send("API Working");
-});
 
-// Export app for Vercel to handle serverless
-export default app;
+
+app.get("/", (req,res)=>{
+    res.send("API Working")
+})
+
+app.listen(port,()=>{
+    console.log(`Server Started on http://localhost:${port}`);
+})
